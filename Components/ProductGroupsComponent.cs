56997f7797e391203cc1d337f13bd186181a94df
Alireza Gharibi <world.war.2.red@gmail.com>
2025-01-08 16:10:33 +0330

    Add files via upload

---
+++ Components/ProductGroupsComponent.cs
@@ -0,0 +1,23 @@
+﻿using HelmonyCornDog.Data;
+using HelmonyCornDog.Data.Repositories;
+using HelmonyCornDog.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelmonyCornDog.Components
+{
+    public class ProductGroupsComponent:ViewComponent
+    {
+       IGroupRepository _groupRepository;
+
+       public ProductGroupsComponent(IGroupRepository groupRepository)
+       {
+           _groupRepository = groupRepository;
+       }
+
+       public async Task<IViewComponentResult> InvokeAsync()
+        {
+           
+            return View("/Views/Components/ProductGroupsComponents.cshtml",_groupRepository.GetGroupForShow());
+        }
+    }
+}
